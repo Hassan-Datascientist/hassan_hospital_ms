d11513c4ee8bb0809ac4bbc634a03a478efd98cd
abdo-aleer <juniorab444@gmail.com>
2025-05-10 19:56:27 +0200

    Added logging path

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register_view
+from .views import register_view, login_user
 
 urlpatterns = [
     path('register/', register_view, name='register'),
+    path('login/', login_user, name='log-in')
 ]
