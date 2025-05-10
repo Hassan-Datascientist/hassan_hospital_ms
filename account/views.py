183c23784918304aba5176b30bc96a25dd4c4d2c
2pac <adwokbrave@gmail.com>
2025-05-10 19:20:13 +0200

    completed some crud operation

--- account/views.py
+++ account/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from account.forms import StaffRegistrationForm,DoctorRegistrationForm  # and DoctorRegistrationForm if you have it
 
+def register_view(request):
+    if request.method == 'POST':
+        submit_type = request.POST.get('submit_type')
 
+        if submit_type == 'staff':
+            staff_form = StaffRegistrationForm(request.POST)
+            doctor_form = DoctorRegistrationForm()  # empty for display
+            if staff_form.is_valid():
+                staff_form.save()
+                return redirect('staff_dashboard')  # or a success page
+        elif submit_type == 'doctor':
+            doctor_form = DoctorRegistrationForm(request.POST)
+            staff_form = StaffRegistrationForm()
+            if doctor_form.is_valid():
+                doctor_form.save()
+                return redirect('staff_dashboard')
+    else:
+        staff_form = StaffRegistrationForm()
+        doctor_form = DoctorRegistrationForm()
 
-
-
-
-
-
-def register(request):
-
-    return render(request, "account/register.html")
-
-
-
-def login_user(request):
-    return render(request, "account/login.html")
+    return render(request, 'account/register.html', {
+        'staff_form': staff_form,
+        'doctor_form': doctor_form,
+    })
--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,11 @@
+# doctor/models.py
 from django.db import models
+from account.models import User
 
-# Create your models here.
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name} ({self.specialization})"
